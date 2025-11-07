66381f546fd50e62b6184c83bff3f6361d32ce5e
Luis Jimenez <luisjimenez@Luiss-MacBook-Air.local>
2025-11-07 14:33:34 -0500

    the bells menu  now shows different images

--- menus/bells_menu.py
+++ menus/bells_menu.py
@@ -1,41 +1,68 @@
 from tkinter import Button, Label, Frame
 from app_state import app_state
 from PIL import Image, ImageTk
-import resources  # ✅ importa resources desde el nivel principal (funciona si ducts es el package principal)
+import resources  
 
 def bells_menu(W, go_back):
-    # Limpiar la ventana
+    
     for widget in W.winfo_children():
         widget.destroy()
-        
-    # ---- TOP FRAME ----
+    
+    
     top_frame = Frame(W, bg='gray5')
     top_frame.pack(side='top', fill='x')
-
+    
+    
     bells_lbl = Label(top_frame, text='Campanas', font=('Arial', 35), bg='gray5', fg='gray60')
     bells_lbl.pack(side='top', pady=1)
-
-    # ---- MIDDLE FRAME ----
+    
+    
     middle_frame = Frame(W, bg='gray10')
     middle_frame.pack(expand=True, fill='both')
+    
+    
+    bell_no_wall_img = resources.load_image("smooth_converging_bellmouth_without_end_wall.png",
+                                    size=(300, 200))  
+    bell_no_wall_lbl = Label(middle_frame, image=bell_no_wall_img, bg='gray5')
+    bell_no_wall_lbl.image = bell_no_wall_img
+    bell_no_wall_lbl.grid(row=0, column=0, padx=20, pady=20)
 
-    # 👉 Cargar imagen desde resources.py (carpeta /images)
-    img_campana = resources.load_image("smooth_converging_bellmouth_without_end_wall.png", size=(300, 200))  # cambia el nombre según tu archivo
-    img_label = Label(middle_frame, image=img_campana, bg='gray10')
-    img_label.image = img_campana  # evita que se borre de memoria
-    img_label.pack(pady=20)
-
-    # ---- BOTTOM FRAME ----
+    bell_with_wall_img = resources.load_image("smooth_converging_bellmouth_with_wall.png",
+                                    size=(300, 200))  
+    bell_with_wall_lbl = Label(middle_frame, image=bell_with_wall_img, bg='gray5')
+    bell_with_wall_lbl.image = bell_with_wall_img
+    bell_with_wall_lbl.grid(row=0, column=1, padx=20, pady=20)
+    
+    
+    conical_bell_no_wall_img = resources.load_image("conical_bell_no_wall.png",
+                                    size=(350, 200))
+    conical_bell_no_wall_lbl = Label(middle_frame, image=conical_bell_no_wall_img, bg='gray5')
+    conical_bell_no_wall_lbl.image = conical_bell_no_wall_img
+    conical_bell_no_wall_lbl.grid(row=0, column=2, padx=20, pady=20)
+    
+    conical_bell_with_wall_img = resources.load_image("conical_bell_with_end_wall.png",
+                                    size=(350, 200))
+    conical_bell_with_wall_lbl = Label(middle_frame, image=conical_bell_with_wall_img, bg='gray5')
+    conical_bell_with_wall_lbl.image = conical_bell_with_wall_img
+    conical_bell_with_wall_lbl.grid(row=0, column=3, padx=20, pady=20)
+    
+    
+    
+    
+    
+    
+    
+    
+    
     bottom_frame = Frame(W, bg='gray5')
     bottom_frame.pack(side='bottom', fill='x')
     
-    back_btn = Button(
-        bottom_frame, text='Regresar',
+    
+    back_btn = Button(bottom_frame, text='Regresar',
         bg='white', fg='black',
         relief='raised',
         activebackground='DodgerBlue2',
         activeforeground='OrangeRed2',
         font=('Arial', 20, 'bold'),
-        command=lambda: go_back(W)
-    )
+        command=lambda: go_back(W))
     back_btn.pack(side='left', padx=10, pady=10)
